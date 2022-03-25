96eff1b2dd0939ca7905753c2ad8e08e3b3ddc1b
Admin <Admin@Dell-PC>
2022-03-25 14:46:47 +0530

    i have done some modification

--- src/test/java/practiceMaven/CreateCampeignTest.java
+++ src/test/java/practiceMaven/CreateCampeignTest.java
@@ -1,5 +1,11 @@
 package practiceMaven;
 
+import org.testng.annotations.Test;
+
 public class CreateCampeignTest {
+	@Test
+	public void createCampeign() {
+		System.out.println("campeign created");
+	}
 
 }
